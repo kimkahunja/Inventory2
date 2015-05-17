8e6d908e5846c33d26ee783c5d3cb43dfe19e984
kimkahunja@gmail.com <kimkahunja@gmail.com>
2015-05-17 18:43:40 +0300

    change in app.js and application.js

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -6,56 +6,5 @@
 
 Ext.application({
     name: 'InventoryApp',
-    extend: 'InventoryApp.Application',
-    views: [
-            'Viewport',
-            'categories.List',
-            'location.Locations',
-            'location.LocationList',
-            'units.UnitList',
-            'product.List',
-            'purchases.PurchaseLanding',
-            'purchases.PurchaseList',
-            'purchases.PurchaseDtlsList'
-        ],
-    controllers: [
-        'App',
-        'Categories',
-        'Locations',
-        'Units',
-        'Products',
-        'Purchases',
-        'PurchasesDtls'
-    ],
-    requires: [
-        'Ext.util.History',
-        'overrides.grid.RowEditor',
-        'Ext.util.Point',
-        'Ext.ux.form.field.ClearButton',
-        'Ext.ux.grid.FilterBar',
-        'InventoryApp.Utilities'
-       // 'kahunja.domain.Proxy',
-       // 'ux/form/field/OperatorButton',
-       // 'ux/grid/column/ActionPro',       
-      //  'ux/grid/AutoResizer'
-    ],
-    autoCreateViewport: true,
-    /**
-     * launch is called immediately upon availability of our app
-     */
-    launch: function( args ) {
-        // "this" = Ext.app.Application
-        var me = this;
-        // init Ext.util.History on app launch; if there is a hash in the url,
-        // our controller will load the appropriate content
-        Ext.util.History.init(function(){
-            var hash = document.location.hash;
-            me.getAppController().fireEvent( 'tokenchange', hash.replace( '#', '' ) );
-        })
-        // add change handler for Ext.util.History; when a change in the token
-        // occurs, this will fire our controller's event to load the appropriate content
-        Ext.util.History.on( 'change', function( token ){
-            me.getAppController().fireEvent( 'tokenchange', token );
-        });
-    }
+    extend: 'InventoryApp.Application',    
 });
--- src/main/webapp/app/Application.js
+++ src/main/webapp/app/Application.js
@@ -4,14 +4,54 @@ Ext.define('InventoryApp.Application', {
     extend: 'Ext.app.Application',
 
     views: [
-        // TODO: add views here
-    ],
-
+            'Viewport',
+            'categories.List',
+            'location.Locations',
+            'location.LocationList',
+            'units.UnitList',
+            'product.List',
+            'purchases.PurchaseLanding',
+            'purchases.PurchaseList',
+            'purchases.PurchaseDtlsList'
+        ],
     controllers: [
-        // TODO: add controllers here
+        'App',
+        'Categories',
+        'Locations',
+        'Units',
+        'Products',
+        'Purchases',
+        'PurchasesDtls'
     ],
-
-    stores: [
-        // TODO: add stores here
-    ]
+    requires: [
+        'Ext.util.History',
+        'overrides.grid.RowEditor',
+        'Ext.util.Point',
+        'Ext.ux.form.field.ClearButton',
+        'Ext.ux.grid.FilterBar',
+        'InventoryApp.Utilities'
+       // 'kahunja.domain.Proxy',
+       // 'ux/form/field/OperatorButton',
+       // 'ux/grid/column/ActionPro',       
+      //  'ux/grid/AutoResizer'
+    ],
+    autoCreateViewport: true,
+    /**
+     * launch is called immediately upon availability of our app
+     */
+    launch: function( args ) {
+        // "this" = Ext.app.Application
+        var me = this;
+        // init Ext.util.History on app launch; if there is a hash in the url,
+        // our controller will load the appropriate content
+        Ext.util.History.init(function(){
+            var hash = document.location.hash;
+            me.getAppController().fireEvent( 'tokenchange', hash.replace( '#', '' ) );
+        })
+        // add change handler for Ext.util.History; when a change in the token
+        // occurs, this will fire our controller's event to load the appropriate content
+        Ext.util.History.on( 'change', function( token ){
+            me.getAppController().fireEvent( 'tokenchange', token );
+        });
+    }
 });
